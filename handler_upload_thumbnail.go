7a375b4c5e40d9d07e3d6da2fb7654f433368abd
Tyler Pearson <tyler.a.pear@gmail.com>
2026-01-14 20:31:27 -0800

    uses base64 strings to store uploaded images in sqlite (bad idea, just for education)

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io"
+	"encoding/base64"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -32,6 +34,48 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse multipart data", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get file metadata", err)
+		return
+	}
+
+	mediaType := header.Header["Content-Type"][0]
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't read file bytes", err)
+		return
+	}
+
+	video_data, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't find video", err)
+		return
+	}
+	if video_data.UserID != userID {
+		respondWithJSON(w, http.StatusUnauthorized, struct{}{})
+		return
+	}
+
+	base64_data := base64.StdEncoding.EncodeToString(data)
+	dataURL := fmt.Sprintf("data:%s;base64,%s", mediaType, base64_data)
+
+	//new_thumbnail := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	video_data.ThumbnailURL = &dataURL
+	err = cfg.db.UpdateVideo(video_data)
+	if err != nil {
+		respondWithError(w, 500, "Couldn't update video thumbnail", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video_data)
+	return
 }
